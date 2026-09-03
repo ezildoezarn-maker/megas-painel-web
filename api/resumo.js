6113557e2a1bd0bc23ff6408331b9a96d1f882f0
ezildoezarn-maker <ezildoezarn@gmail.com>
2026-09-03 09:24:26 +0200

    Add API handler for resumo endpoint

---
+++ api/resumo.js
@@ -0,0 +1,11 @@
+const BOT_URL = "http://project.metahosting.shop:2006";
+
+module.exports = async function handler(req, res) {
+  try {
+    const r = await fetch(`${BOT_URL}/api/resumo`);
+    const data = await r.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(502).json({ erro: "bot indisponível", detalhe: String(e) });
+  }
+}
